e8b2fb28979e65805f15ad563569c89fcfdd8c57
Vitaly <vitaly@example.com>
2024-05-14 19:12:30 +0300

    чистка класса контроллера, удаление класса сервис

--- Controller/HotelServiceVerificationController.cs
+++ Controller/HotelServiceVerificationController.cs
@@ -1,28 +1,54 @@
 using HotelCalcApp.Model.Data;
-using HotelCalcApp.Model.Services;
 
 namespace HotelCalcApp.Controller
 {
     /// <summary>Контроллер Сервиса для проверки  сервиса отеля</summary>
     public class HotelServiceVerificationController
     {
-        /// <summary>Экземпляр Сервиса для проверки  сервиса отеля</summary>
-        private HotelServiceVerificationModelService _HotelServiceVerificationService;
 
-        /// <summary>Экземпляр Сервиса для проверки  сервиса отеля</summary>
-        public HotelServiceVerificationModelService HotelServiceVerificationService
+        private Hotel _CurrentHotel;
+        private HotelService _HotelServiceForVerification;
+        private ServiceSaleStop _CurrentStopSaleService;
+
+        public Hotel CurrentHotel
         {
             get
             {
-                return _HotelServiceVerificationService;
+                return _CurrentHotel;
             }
             set
             {
-                _HotelServiceVerificationService = value;
+                _CurrentHotel = value;
             }
         }
 
-        /// <summary>Экземпляр Контроллера Сервиса для проверки  сервиса отеля</summary>
+        /// <summary>Проверяемый сервис отеля</summary>
+        public HotelService HotelServiceForVerification
+        {
+            get
+            {
+                return _HotelServiceForVerification;
+            }
+            set
+            {
+                _HotelServiceForVerification = value;
+            }
+        }
+
+        /// <summary>Актуальный период Стоп Продаж сервис отеля</summary>
+        public ServiceSaleStop CurrentStopSaleService
+        {
+            get
+            {
+                return _CurrentStopSaleService;
+            }
+            set
+            {
+                _CurrentStopSaleService = value;
+            }
+        }
+
+
         public HotelServiceVerificationController() { }
 
         // TODO:    1.  Определить актуальную дату периодов сервисов отеля статуса - Стоп Продаж;
